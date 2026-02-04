15a3643a0450221fd3c51ba0abdee8ff4070ac95
Faizan-313 <faizansyedpeer@gmail.com>
2026-02-04 20:23:37 +0530

    asdf

---
+++ backend/dist/types/types.js
@@ -0,0 +1,7 @@
+export var GamePhase;
+(function (GamePhase) {
+    GamePhase["LOBBY"] = "LOBBY";
+    GamePhase["DISCUSSION"] = "DISCUSSION";
+    GamePhase["VOTING"] = "VOTING";
+    GamePhase["RESULT"] = "RESULT";
+})(GamePhase || (GamePhase = {}));
